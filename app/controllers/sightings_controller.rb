8f31b51527b09153141617dafcff06c5d16b687b
github username <github email address>
2020-06-07 16:37:52 -0700

    Done.

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,9 @@
+class SightingsController < ApplicationController
+
+    def show
+    sighting = Sighting.find_by(id: params[:id])
+    render json: { id: sighting.id, bird: sighting.bird, location: sighting.location }
+    end
+
+
+end
