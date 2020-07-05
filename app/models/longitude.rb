13cec2e78e04d57369d80f68492db17f6992723a
github username <github email address>
2020-07-05 18:27:57 -0500

    Done.

---
+++ app/models/longitude.rb
@@ -0,0 +1,4 @@
+class Longitude < ApplicationRecord
+  belongs_to :bird
+  belongs_to :location
+end
---
+++ test/models/longitude_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LongitudeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
