abeb43a529ffbc41455b84b66557a8816a733299
Anthony Tilelli <Anthony@tilelli.me>
2020-07-21 23:37:29 -0400

    Done.

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module BirdWatcher
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :options]
+        resource '*', headers: :any, methods: %i[get options]
       end
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_032831) do
-
-  create_table "birds", force: :cascade do |t|
-    t.string "name"
-    t.string "species"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema.define(version: 20_200_722_032_831) do
+  create_table 'birds', force: :cascade do |t|
+    t.string 'name'
+    t.string 'species'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "locations", force: :cascade do |t|
-    t.float "latitude"
-    t.float "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'locations', force: :cascade do |t|
+    t.float 'latitude'
+    t.float 'longitude'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "sightings", force: :cascade do |t|
-    t.integer "bird_id"
-    t.integer "location_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["bird_id"], name: "index_sightings_on_bird_id"
-    t.index ["location_id"], name: "index_sightings_on_location_id"
+  create_table 'sightings', force: :cascade do |t|
+    t.integer 'bird_id'
+    t.integer 'location_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['bird_id'], name: 'index_sightings_on_bird_id'
+    t.index ['location_id'], name: 'index_sightings_on_location_id'
   end
-
 end
