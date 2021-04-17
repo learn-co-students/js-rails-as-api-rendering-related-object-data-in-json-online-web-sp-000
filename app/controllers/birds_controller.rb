4ad56a3436f5611324375409e914572fa0f814f6
github username <github email address>
2021-04-17 14:35:40 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,12 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    sightings = Sighting.all
+    render json: sightings.to_json(include:[:bird,:location])
+  end
+ def show
+  sighting= Sighting.fnd_by(id: params[:id])
+    render json: sighting.to_json(include:[:bird, :location])
+ else render json: {message: 'No sighting found with that id'}
+ end
   end
 end
